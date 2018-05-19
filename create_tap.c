5f07ea376e0dbfbab61d621ecf2ac6b3b4c8e43e
ortymd <polyarush.dmytro@gmail.com>
2018-05-19 14:46:08 +0300

    added simple tests

--- create_tap.c
+++ create_tap.c
@@ -1,40 +1,11 @@
-#include <linux /if.h>
-#include <linux /if_tun.h>
-
-int get_net_fd(void) {
-
-  struct ifreq ifr;
-  int fd, err;
-  char *clonedev = "/dev/net/tun";	/* this block device is clone device as used
-																			 to create any tun/tap interface */
-  /* Arguments taken by the function:
-   *
-   * char *dev: the name of an interface (or '\0'). MUST have enough
-   *   space to hold the interface name if '\0' is passed
-   * int flags: interface flags (eg, IFF_TUN etc.)
-   */
-
-   /* open the clone device */
-   if( (fd = open(clonedev, O_RDWR)) < 0 ) {
-     return fd;
-   }
-
-   memset(&ifr, 0, sizeof(ifr));
-
-   ifr.ifr_flags = IFF_TAP;   /* TAP device is neede to read/write eth frames */
-
-   if( (err = ioctl(fd, TUNSETIFF, (void *) &ifr)) < 0 ) {
-     close(fd);
-     return err;
-   }
-
-  /* this is the special file descriptor that the caller will use to read/write 
-   *  virtual interface */
-  return fd;
-}
+#include <func.h>
+#include <net/ethernet.h>
+#include <stdlib.h>
 
 int main()
 {
-	int fd = get_net_fd();
-	return 0;
+	test1();
+	test2();
+	exit(0);
 }
+
--- func.h
+++
@@ -1,1 +0,0 @@
-int get_net_fd(void);		/* returns file descriptor to use to read/write to network */
